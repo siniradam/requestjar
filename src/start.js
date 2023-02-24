30867743df75275eab2aa34a968c267c5cd059e9
Koray K <3178435+siniradam@users.noreply.github.com>
2023-02-24 14:55:14 -0800

    renames

--- src/start.js
+++ src/start.js
@@ -1,12 +1,11 @@
 const { logArray } = require("./utils");
 
+const info = ["Thanks for trying RequestJar", "Starting in Http Mode:"];
+logArray(info, "info");
+logArray(["npm start serve"], "code");
+logArray(["Start in Https Mode:"], "info");
+logArray(["npm start serves"], "code");
 
-const info = ["Thanks for trying PostBIN","Starting in Http Mode:"]
-logArray(info,"info")
-logArray(["npm start serve"],"code")
-logArray(["Start in Https Mode:"],"info")
-logArray(["npm start serves"],"code")
-
-const moreinfo = ["For more info and tips","you can use these commands"]
-logArray(moreinfo,"info")
-logArray(["npm start help","npm start tips"],"code")
+const moreinfo = ["For more info and tips", "you can use these commands"];
+logArray(moreinfo, "info");
+logArray(["npm start help", "npm start tips"], "code");
