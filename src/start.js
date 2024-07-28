d09f9ebdd643793da3433abcf9a55cd883a28afd
Koray K <3178435+siniradam@users.noreply.github.com>
2024-07-28 18:22:16 +0300

    head method

--- src/start.js
+++ src/start.js
@@ -2,10 +2,10 @@ const { logArray } = require("./utils");
 
 const info = ["Thanks for trying RequestJar", "Starting in Http Mode:"];
 logArray(info, "info");
-logArray(["npm start serve"], "code");
+logArray(["npm run serve"], "code");
 logArray(["Start in Https Mode:"], "info");
-logArray(["npm start serves"], "code");
+logArray(["npm run serves"], "code");
 
 const moreinfo = ["For more info and tips", "you can use these commands"];
 logArray(moreinfo, "info");
-logArray(["npm start help", "npm start tips"], "code");
+logArray(["npm run help", "npm run tips"], "code");
